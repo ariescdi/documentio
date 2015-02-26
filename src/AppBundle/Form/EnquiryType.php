973b2c98e259ee8518c2ce6eb48476391b477b6a
neyret <thibaud.neyret@gmail.com>
2015-02-26 16:45:31 +0100

    add footer and contact page

---
+++ src/AppBundle/Form/EnquiryType.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class EnquiryType extends AbstractType{
+    public function buildForm(FormBuilderInterface $builder, array $options){
+        $builder->add('name', 'text', array('label' => "Votre nom (requis) :"));
+        $builder->add('email', 'email', array('label' => "Votre adresse mail (requis) :"));
+        $builder->add('subject','text' , array('label' => "Votre objet (requis) :"));
+        $builder->add('body', 'textarea' ,array('label' => "Votre message (requis) :") );
+        $builder->add('submit', 'submit');
+    }
+
+    public function getName()
+    {
+        return 'contact';
+    }
+}
