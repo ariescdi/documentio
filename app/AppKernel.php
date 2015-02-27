b684bee12c3d3d8b3cca9f25fcbe34179cabaa46
christophe joly <tof3d@hotmail.fr>
2015-02-27 10:56:29 +0100

    Add theming, with Craue config bundle
    Fixtures to add default theme configuration.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new MediaBundle\MediaBundle(),
             new UserBundle\UserBundle(),
+            new Craue\ConfigBundle\CraueConfigBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
