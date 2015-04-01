93b3aaa6e2d92799ddf8ad801a8007cc1a6ddc5f
Alexandre Petrone <alexandre_petrone@yahoo.fr>
2015-04-01 19:02:04 +0200

    Add Scenario

--- src/MediaBundle/Tests/Controller/MediaControllerTest.php
+++ src/MediaBundle/Tests/Controller/MediaControllerTest.php
@@ -6,50 +6,98 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MediaControllerTest extends WebTestCase
 {
-    /*
     public function testCompleteScenario()
     {
-        // Create a new client to browse the application
+        //ON CREER UN CLIENT POUR VISUALISE L'APPLICATION
         $client = static::createClient();
 
-        // Create a new entry in the database
-        $crawler = $client->request('GET', '/media/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /media/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
+        //ON AUTORISE LES REDIRECTIONS
+        $client->followRedirects();
 
-        // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'mediabundle_media[field_name]'  => 'Test',
-            // ... other fields to fill
+        //ON ARRIVE SUR LE SITE
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /");
+
+        //ON VA SUR MEDIA
+        $crawler = $client->click($crawler->selectLink('Médias')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /media");
+
+        //ON VISUALISE LE DOCUMENT KDKDK
+        $crawler = $client->click($crawler->selectLink('kdkdk')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /kdkdk");
+        //$crawler = $client->click($crawler->selectLink('Télécharger kdkdk')->link());
+
+
+        //ON SE REND SUR LA PAGE DE CONNEXION
+        $crawler = $client->click($crawler->selectLink('Admin')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /login");
+
+        //ON REMPLI LE FORMULAIRE DE CONNEXION
+        $form = $crawler->selectButton('_submit')->form(array(
+            '_username'  => 'admin',
+            '_password'  => 'admin',
         ));
 
         $client->submit($form);
-        $crawler = $client->followRedirect();
 
-        // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
+        //ON SE REND SUR LA PAGE D'ADMINISTRATION
+        $crawler = $client->request('GET', '/admin');
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin");
 
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
+        //VERIFICATION DE L'AFFICHAGE DES STATS
+        $this->assertGreaterThan(0, $crawler->filter('td:contains("user")')->count(), 'Missing element td:contains("user")');
 
-        $form = $crawler->selectButton('Update')->form(array(
-            'mediabundle_media[field_name]'  => 'Foo',
-            // ... other fields to fill
+        //ON SE REND SUR LA LISTE DES MEDIAS (ADMIN)
+        $crawler = $client->click($crawler->selectLink('Liste des médias')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/media/");
+
+        //ON VERIFIE QUE LE DOCUMENT "USER DOC" EST PRESENT
+        $this->assertGreaterThan(0, $crawler->filter('td:contains("user doc")')->count(), 'Missing element td:contains("user doc")');
+
+        //ON SE REND SUR LA PAGE D'AJOUT D'UN MEDIA
+        $crawler = $client->click($crawler->selectLink('Ajouter un média')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /admin/media/new");
+
+        //ON REMPLI LE FORMULAIRE
+        $form = $crawler->selectButton('mediabundle_media[submit]')->form(array(
+            'mediabundle_media[name]'  => 'Test',
+            'mediabundle_media[comment]' => 'Test',
         ));
 
+        $form['mediabundle_media[category]']->select(3);
+        $form['mediabundle_media[type]']->select(2);
+        $form['mediabundle_media[isPublished]']->tick();
+        $form['mediabundle_media[file]']->upload('/URL/twitter.png');
+
         $client->submit($form);
-        $crawler = $client->followRedirect();
 
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
+        // A VERIFIER POURQUOI APPELER LA PAGE /!\ /!\
+        $crawler = $client->request('GET', '/admin/media/12');
 
-        // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
+        //ON VERIFIE QUE LE DOCUMENT EST PRESENT SUR LE SITE
+        $crawler = $client->click($crawler->selectLink('Voir le document sur le site')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /media/Test");
 
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        //ON EDITE CE NOUVEAU DOCUMENT
+        $crawler = $client->click($crawler->selectLink('Editer')->link());
+        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET admin/media/{{id}}/edit");
+
+
+        $form = $crawler->selectButton('mediabundle_media[submit]')->form(array(
+            'mediabundle_media[name]'  => 'Test modification',
+        ));
+
+        $form['mediabundle_media[category]']->select(1);
+        $form['mediabundle_media[type]']->select(1);
+        $form['mediabundle_media[isPublished]']->tick();
+
+        $client->submit($form);
+
+
+        //SUPPRESSION DE CE NOUVEAU DOCUMENT
+        $crawler = $client->click($crawler->selectLink('Supprimer')->link());
+        $crawler = $client->request('GET', '/media/Test');
+        $this->assertEquals(404, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /media/Test");
+
     }
-
-    */
 }
